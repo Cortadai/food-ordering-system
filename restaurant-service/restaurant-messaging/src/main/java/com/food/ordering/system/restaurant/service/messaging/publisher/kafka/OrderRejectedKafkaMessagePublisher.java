668e10f66dd818e713b01987b447e7571a21ba51
David Cortaberria <david.cortabarria@gmail.com>
2025-04-20 11:39:19 +0200

    restaurant-service module completed plus some minor refactoring

---
+++ restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/publisher/kafka/OrderRejectedKafkaMessagePublisher.java
@@ -0,0 +1,52 @@
+package com.food.ordering.system.restaurant.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
+import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import com.food.ordering.system.restaurant.service.domain.config.RestaurantConfigData;
+import com.food.ordering.system.restaurant.service.domain.event.OrderRejectedEvent;
+import com.food.ordering.system.restaurant.service.domain.ports.output.message.publisher.OrderRejectedMessagePublisher;
+import com.food.ordering.system.restaurant.service.messaging.mapper.RestaurantMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderRejectedKafkaMessagePublisher implements OrderRejectedMessagePublisher {
+
+    private final RestaurantMessagingDataMapper restaurantMessagingDataMapper;
+    private final KafkaProducer<String, RestaurantApprovalResponseAvroModel> kafkaProducer;
+    private final RestaurantConfigData restaurantConfigData;
+    private final KafkaMessageHelper kafkaMessageHelper;
+
+    public OrderRejectedKafkaMessagePublisher(RestaurantMessagingDataMapper restaurantMessagingDataMapper,
+                                              KafkaProducer<String, RestaurantApprovalResponseAvroModel> kafkaProducer,
+                                              RestaurantConfigData restaurantConfigData,
+                                              KafkaMessageHelper kafkaMessageHelper) {
+        this.restaurantMessagingDataMapper = restaurantMessagingDataMapper;
+        this.kafkaProducer = kafkaProducer;
+        this.restaurantConfigData = restaurantConfigData;
+        this.kafkaMessageHelper = kafkaMessageHelper;
+    }
+
+    @Override
+    public void publish(OrderRejectedEvent orderRejectedEvent) {
+        String orderId = orderRejectedEvent.getOrderApproval().getOrderId().getValue().toString();
+        log.info("Received OrderRejectedEvent for order id: {}", orderId);
+        try{
+            RestaurantApprovalResponseAvroModel restaurantApprovalResponseAvroModel =
+                    restaurantMessagingDataMapper.orderRejectedEventToRestaurantApprovalResponseAvroModel(orderRejectedEvent);
+            kafkaProducer.send(restaurantConfigData.getRestaurantApprovalRequestTopicName(),
+                    orderId,
+                    restaurantApprovalResponseAvroModel,
+                    kafkaMessageHelper.getKafkaCallBack(restaurantConfigData.getRestaurantApprovalResponseTopicName(),
+                            restaurantApprovalResponseAvroModel, orderId,
+                            "RestaurantApprovalResponseAvroModel"));
+            log.info("RestaurantApprovalResponseAvroModel has been sent to kafka at: {}", System.nanoTime());
+        } catch (Exception e){
+            log.error("Error while sending RestaurantApprovalResponseAvroModel to kafka" +
+                    "with order id: {}, error: {}", orderId, e.getMessage());
+        }
+    }
+
+}
